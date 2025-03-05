f22aec4b20a5d77e0977c40e0a4b1144ed30c685
Nithish3115 <nithish031105@gmail.com>
2025-03-05 15:56:14 +0530

    Melodicgen

--- app.py
+++ app.py
@@ -28,5 +28,5 @@ def generate():
         return jsonify({'error': 'Failed to generate melody'}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT",5000)))
 
